009b251f18398180b7096af1c85c4de5e17dae86
tqm <2631260229@qq.com>
2020-04-10 18:03:57 +0800

    app update

--- src/app.tsx
+++ src/app.tsx
@@ -26,13 +26,43 @@ class App extends Component {
    */
   config: Config = {
     pages: [
-      'pages/home/home'
+      'pages/home/home',
+      'pages/cate/cate',
+      'pages/cart/cart',
+      'pages/user/user',
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: '#666',
+      selectedColor: '#b4282d',
+      backgroundColor: "#fafafa",
+      borderStyle: 'black',
+      list: [{
+        pagePath: 'pages/home/home',
+        iconPath: 'assets/tab-bar/home.png',
+        selectedIconPath: 'assets/tab-bar/home-active.png',
+        text: '首页',
+      }, {
+        pagePath: 'pages/cate/cate',
+        iconPath: 'assets/tab-bar/cate.png',
+        selectedIconPath: 'assets/tab-bar/cate-active.png',
+        text: '分类',
+      }, {
+        pagePath: 'pages/cart/cart',
+        iconPath: 'assets/tab-bar/cart.png',
+        selectedIconPath: 'assets/tab-bar/cart-active.png',
+        text: '购物车'
+      }, {
+        pagePath: 'pages/user/user',
+        iconPath: 'assets/tab-bar/user.png',
+        selectedIconPath: 'assets/tab-bar/user-active.png',
+        text: '个人'
+      }]
     }
   }
 
